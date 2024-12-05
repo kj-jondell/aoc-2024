52877b2f529df73c48275d37400b2d31b904f50c
Karl Johannes Jondell <karl_johannes.jondell@sr.se>
2024-12-05 12:41:09 +0100

    it aint pretty

--- python/day-5/__main__.py
+++ python/day-5/__main__.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 def get_cond():
     while (line := next(sys.stdin).strip()):
@@ -9,5 +10,19 @@ conditions=[(int(x), int(y)) for x, y in get_cond()]
 q=[[int(x.strip()) for x in line.split(",")] for line in sys.stdin]
 s=[[u in conditions for u in zip(i, i[1:])] for i in q]
 
-s=sum([q[i][int(len(q[i])/2)] for i, k in enumerate(s) if False not in k])
-print(s)
+#s=sum([q[i][int(len(q[i])/2)] for i, k in enumerate(s) if False not in k])
+#print(s)
+
+s = [(q[i],[o for o,b in enumerate(k) if not b]) for i, k in enumerate(s) if False in k]
+k = 0
+for x, i in s:
+    while True:
+        for u in i:
+            x = x[:u] + x[u:u+2][::-1] + x[u+2:]
+        s=[u in conditions for u in zip(x, x[1:])]
+        if False not in s:
+            k += x[int(len(x)/2)]
+            break
+        else:
+            i = [o for o,b in enumerate(s) if not b]
+print(k)
